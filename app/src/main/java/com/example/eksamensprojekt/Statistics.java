956dc6b6f3b493d8c160ad5c51b4a6efbd4c1a29
Groed <Johan.hammer.jakobsen@gmail.com>
2020-05-28 18:45:19 +0200

    opdatering af graphview version

--- app/src/main/java/com/example/eksamensprojekt/Statistics.java
+++ app/src/main/java/com/example/eksamensprojekt/Statistics.java
@@ -7,7 +7,10 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eksamensprojekt.databasecomp.AppDatabase;
+import com.example.eksamensprojekt.databasecomp.Weight;
+import com.example.eksamensprojekt.databasecomp.User;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Statistics extends AppCompatActivity {
 private AppDatabase db;
@@ -17,6 +20,10 @@ private AppDatabase db;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
         db = AppDatabase.getAppDatabase(this);
+        LineGraphSeries<Weight> series = new LineGraphSeries<Weight>(new Weight[] {
+
+        })
+
 
         Button buttonBack = findViewById(R.id.button9);
         buttonBack.setOnClickListener(new View.OnClickListener() {
